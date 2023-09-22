d4524cfb6c2460a33b5282a9360d4f6b87097858
Larissa Soares Silva <larissa.ssilva@gmail.com>
2023-09-21 22:00:34 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,22 +3,33 @@ import java.io.IOException;
 
 public class Main {
  
+    /*public static void main(String[] args) throws IOException {
+        int A=0, B=0, C=0, area=0;
+        area=A*B;
+        C=(area/100)*20;
+        System.out.println(A+" não eh primo ");
+    } */
     public static void main(String[] args) throws IOException {
-        int X=0, c=0;
-        try (Scanner sc = new Scanner(System.in)) {
+        int X=0, c=0, N=0;
+        Scanner sc = new Scanner(System.in);
+        N=sc.nextInt();
+                   
+        for(int j=0;j<N;j++){
+           c=0;
             X=sc.nextInt();
-        }       
-        for(int i=1; i<=X;i++){
-            if(X%i==0){
-                c=c+1;
+                   
+            for(int i=1; i<=X;i++){
+                if(X%i==0){
+                    c=c+1;
+                }
             }
-        }
-        if(c==2){
-            System.out.println(X+" eh primo ");
-        }else{
-            System.out.println(X+" nao eh primo ");
-        }
-    } 
+            if(c==2){
+                System.out.println(X+" eh primo");
+            }else{
+                System.out.println(X+" nao eh primo");
+            }
+        } 
+    }
     
 }
-    
+  
